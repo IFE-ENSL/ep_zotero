45858d7abc92c467fd1ef2b90914a00cd13949f0
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2014-01-29 11:37:21 +0100

    Allow a user to delete his ids stored in local

--- static/js/modals/form.js
+++ static/js/modals/form.js
@@ -22,10 +22,23 @@ function createApiZoteroFormModal() {
     );
 
     // create the form
+    var checked = "";
+    var userIdValue = localStorage.getItem('zoteroApiUserId');
+    var userKeyValue = localStorage.getItem('zoteroApiUserKey');
+    if (localStorage.getItem('zoteroApiUserId')){
+        checked = "checked";
+    } else {
+        userIdValue = "";
+        userKeyValue = "";
+    }
     var $apiZoteroForm = jQuery(
         '<form>'+
-            '<p><label>Votre identifiant API (userID)</label><input type="text" name="user_api_id" value="'+zoteroApiUserId+'"/></p>'+
-            '<p><label>Votre clé API (api key)</label><input type="text" name="user_api_key" value="'+zoteroApiUserKey+'"/></p>'+
+            '<p><label>Votre identifiant API (userID)</label><input type="text" name="user_api_id" value="'+userIdValue+'"/></p>'+
+            '<p><label>Votre clé API (api key)</label><input type="text" name="user_api_key" value="'+userKeyValue+'"/></p>'+
+            '<p class="storage">'+
+                '<label class="save-ids">Sauvegarder mes identifiants pour cet ordinateur</label>'+
+                '<input type="checkbox" id="save_ids" value="1" '+checked+'>'+
+            '</p>'+
         '</form>'
     );
     $formModal.find('.modal-body').append($apiZoteroForm);
@@ -34,7 +47,7 @@ function createApiZoteroFormModal() {
     var $helpLink = jQuery('<p><em class="help-link"><a class="help-link" href="#">Comment récupérer ces identifiants?</a></em></p>');
     $apiZoteroForm.append($helpLink);
 
-    $helpLink.on('click', function() {
+    $helpLink.find('a').on('click', function() {
         $formModal.modal('hide');
         $helpModal = createHelpModal();
         jQuery('body').append($helpModal);
@@ -50,8 +63,10 @@ function createApiZoteroFormModal() {
         // set user id and key
         zoteroApiUserId = $apiZoteroForm.find('*[name="user_api_id"]').val();
         zoteroApiUserKey = $apiZoteroForm.find('*[name="user_api_key"]').val();
-        localStorage.setItem('zoteroApiUserKey', zoteroApiUserKey);
-        localStorage.setItem('zoteroApiUserId', zoteroApiUserId);
+        if (document.getElementById('save_ids').checked){
+            localStorage.setItem('zoteroApiUserKey', zoteroApiUserKey);
+            localStorage.setItem('zoteroApiUserId', zoteroApiUserId);
+        }
         var url = "https://api.zotero.org/users/"+zoteroApiUserId+"/groups?key="+zoteroApiUserKey;
         console.log(url);
         jQuery.ajax({
@@ -71,7 +86,17 @@ function createApiZoteroFormModal() {
         });
     });
 
-    $formModal.on('hidden.bs.modal', function (e) {
+    var $deleteLocalStorageButton = jQuery('<button type="button" class="btn btn-default delete-ids">Supprimer mes identifiants</button>');
+    $formModal.find('p.storage').append($deleteLocalStorageButton);
+    $deleteLocalStorageButton.on('click', function() {
+        /*$apiZoteroForm.find('*[name="user_api_id"]').attr('value', '');
+        $apiZoteroForm.find('*[name="user_api_key"]').attr('value', '');*/
+        console.log("delta");
+        localStorage.clear();
+        document.getElementById("save_ids").checked = false;
+    });
+
+    $formModal.on('hidden.bs.modal', function () {
         jQuery(this).remove();
     });
 
